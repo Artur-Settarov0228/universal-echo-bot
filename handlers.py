e5b878df58a0a174262022d0273e672af39809a7
settarovartur <settarovartur2@gamil.com>
2025-08-05 21:05:34 +0500

    init

--- handlers.py
+++ handlers.py
@@ -1,7 +1,51 @@
-from telegram import Update, ParseMode
+from telegram import Update, ParseMode, KeyboardButton, ReplyKeyboardMarkup, WebAppInfo
 from telegram.ext import CallbackContext
 
-def hanler_photo(update : Update, context : CallbackContext):
+
+
+def handler_main_menu(update: Update, context: CallbackContext):
+    update.message.reply_markdown_v2(
+        text="> Siz *Bosh Menu* ni tanladingiz",
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [KeyboardButton('Mahsulotlar'), KeyboardButton('Texnikalar')],
+                [KeyboardButton('Modellari')]
+            ],
+            resize_keyboard=True,
+            one_time_keyboard=True,
+        )
+    )
+
+def start(update: Update, context: CallbackContext):
+    bot = context.bot
+    user = update.effective_user
+    bot.send_message(
+        chat_id=user.id,
+        text='Menu tanlang:',
+        reply_markup=ReplyKeyboardMarkup(
+            keyboard=[
+                [
+                    KeyboardButton(
+                                'Bosh Menu',
+                                web_app=WebAppInfo(url="https://kun.uz")
+                                ),
+                  KeyboardButton('Buyurtma berish')
+                  ],
+
+                [KeyboardButton('Location', request_location=True), KeyboardButton('Contact', request_contact=True)],
+                
+                [
+                    KeyboardButton('My gov',
+                                   web_app=WebAppInfo(url="https://my.gov.uz/")
+                        )
+                ]
+            ],
+            resize_keyboard=True
+        )
+    )
+
+
+def handler_photo(update : Update, context : CallbackContext):
     photo = update.message.photo[-1]
     update.message.reply_photo(
         photo=photo,
@@ -9,7 +53,7 @@ def hanler_photo(update : Update, context : CallbackContext):
         parse_mode=ParseMode.HTML
     )
 
-def hanler_text(update : Update , context : CallbackContext):
+def handler_text(update : Update , context : CallbackContext):
     update.message.reply_markdown_v2(
         text= f"linkni ustiga bosing [inline URL](http://www.example.com/)"
     )
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from config import TOKEN
-from telegram.ext import Updater, MessageHandler, Filters
-from handlers import hanler_photo, hanler_text
+from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
+from handlers import handler_photo, handler_main_menu, start, handler_text
 
 
 def main() -> None:
     updater = Updater(TOKEN)
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(MessageHandler(Filters.photo, hanler_photo))
-    dispatcher.add_handler(MessageHandler(Filters.text, hanler_text))
+    dispatcher.add_handler(CommandHandler(['start', 'boshlash'], start))
+
+    dispatcher.add_handler(MessageHandler(Filters.photo, handler_photo))
+    dispatcher.add_handler(MessageHandler(Filters.text('Buyurtma berish'), handler_main_menu))
+    dispatcher.add_handler(MessageHandler(Filters.text, handler_text))
 
     updater.start_polling()
     updater.idle()
